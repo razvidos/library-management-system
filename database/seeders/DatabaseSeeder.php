03c5d6ddb7724cb1b403354429bb491fda36eccb
ihor.solomianyi <ihor.solomianyi@appflame.com>
2023-01-03 18:47:23 +0200

    Init Laravel 9 vue 3 vuetify 3

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'name' => 'Admin',
+            'password' => '12345678',
+            'email' => 'admin@gmail.com'
+        ]);
+    }
+}
---
+++ resources/js/app.js
@@ -0,0 +1,22 @@
+import './bootstrap';
+import { createApp } from 'vue';
+
+
+const app = createApp({});
+
+import ExampleComponent from './components/ExampleComponent.vue';
+app.component('example-component', ExampleComponent);
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
+//     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
+// });
+
+app.mount('#app');
